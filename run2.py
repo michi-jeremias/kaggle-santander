c3e2e9425a69eff22a89600709aa6b7d3239627a
michael.jeremias <michael.jeremias.home@gmail.com>
2022-03-30 12:43:46 +0200

    Update run2.py

--- run2.py
+++ run2.py
@@ -26,19 +26,19 @@ loss_fn = nn.BCELoss()
 
 # Hyperparameters
 hparam = {
-    "batchsize": [128, 1024],
+    "batchsize": [128, 256],
     "lr": [2e-3, 2e-4]
 }
-hparam = {
-    "batchsize": [128],
-    "lr": [2e-4]
-}
+# hparam = {
+#     "batchsize": [128],
+#     "lr": [2e-4]
+# }
 
 hyperparameter = Hyperparameter(hparam)
 
 #################
 
-experiment = next(hyperparameter.get_experiments())
+# experiment = next(hyperparameter.get_experiments())
 
 for experiment in hyperparameter.get_experiments():
 
@@ -62,38 +62,38 @@ for experiment in hyperparameter.get_experiments():
     )
 
     console_reporter = ConsoleReporter()
-    console_reporter.add_metrics([BinaryCrossentropy(), RocAuc()])
+    console_reporter.add_metrics([BinaryCrossentropy()])
 
     tbscalar_reporter = TensorboardScalarReporter(hparam=experiment)
-    tbscalar_reporter.add_metrics([BinaryCrossentropy(), RocAuc()])
+    tbscalar_reporter.add_metrics([BinaryCrossentropy()])
 
-    console_reporter_val = ConsoleReporter()
-    console_reporter_val.add_metrics(BinaryCrossentropy())
+    # console_reporter_val = ConsoleReporter()
+    # console_reporter_val.add_metrics(BinaryCrossentropy())
 
     tbscalar_reporter_val = TensorboardScalarReporter(hparam=experiment)
     tbscalar_reporter_val.add_metrics(BinaryCrossentropy())
 
-    tbhparam_reporter = TensorboardHparamReporter(hparam=experiment)
-    tbhparam_reporter.add_metrics([BinaryCrossentropy()])
+    # tbhparam_reporter = TensorboardHparamReporter(hparam=experiment)
+    # tbhparam_reporter.add_metrics([BinaryCrossentropy()])
 
     TRAINER = Trainer(
         loader=train_loader,
         optimizer=optimizer,
         loss_fn=loss_fn,
         batch_reporters=tbscalar_reporter,
-        epoch_reporters=console_reporter
+        # epoch_reporters=console_reporter
     )
 
     VALIDATOR = Validator(
         loader=val_loader,
-        batch_reporters=[console_reporter_val, tbscalar_reporter_val],
+        batch_reporters=tbscalar_reporter_val,
     )
 
     RUNNER = Runner(
         model=model,
         trainer=TRAINER,
-        validator=VALIDATOR,
-        run_reporters=tbhparam_reporter
+        # validator=VALIDATOR,
+        # run_reporters=tbhparam_reporter
     )
 
     RUNNER.run(4)
